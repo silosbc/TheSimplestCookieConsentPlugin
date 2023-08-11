b90a33f68d70f2a7e1c41ed8a6086da5a915d8fb
silosbc <141883892+silosbc@users.noreply.github.com>
2023-08-11 20:26:41 +0200

    Update consent.php

--- consent.php
+++ consent.php
@@ -3,7 +3,7 @@
 if(empty($_COOKIE['thesimplestcookieconsentplugin']))
 echo '<script type="text/javascript" >
 var cconsent=confirm("Hello! This site appears to use cookies necessary to ensure its proper functioning and tracking cookies to understand how you interact with it. If you accept this, press OK to go to the page. Otherwise, press Cancel.");
-if(cconsent) document.cookie = "thesimplestcookieconsentplugin=accepted; expires=Wed, 05 Aug 2040 23:00:00 UTC";
+if(cconsent) document.cookie = "thesimplestcookieconsentplugin=accepted; expires=Wed, 05 Aug 2100 23:00:00 UTC";
 else
 window.location.href="about:blank";
 </script>';
