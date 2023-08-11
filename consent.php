c3b05e305c50d790b4f18a1c33742475a9869f47
silosbc <141883892+silosbc@users.noreply.github.com>
2023-08-11 07:27:56 +0200

    Add files via upload

--- consent.php
+++ consent.php
@@ -1,6 +1,5 @@
 <?php
-if(!(str_contains(strtolower($_SERVER['HTTP_USER_AGENT']),strtolower('google'))||str_contains(strtolower($_SERVER['HTTP_USER_AGENT']),strtolower('bing'))))
-{
+
 if($_COOKIE['cookieconsent']!='true')
 echo '<script type="text/javascript" >
 var cconsent=confirm("Hello! This site appears to use cookies necessary to ensure its proper functioning and tracking cookies to understand how you interact with it. If you accept this, press OK to go to the page. Otherwise, press Cancel.");
@@ -8,6 +7,6 @@ if(cconsent) document.cookie = "cookieconsent=true; expires=Wed, 05 Aug 2040 23:
 else
 window.location.href="about:blank";
 </script>';
-}
+
 
 ?>
