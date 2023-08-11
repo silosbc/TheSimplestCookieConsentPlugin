3996fbff29a1680eb3377f7de00fae8bba906d44
silosbc <141883892+silosbc@users.noreply.github.com>
2023-08-11 06:59:42 +0200

    Add files via upload

--- consent.php
+++ consent.php
@@ -3,7 +3,7 @@ if(!(str_contains(strtolower($_SERVER['HTTP_USER_AGENT']),strtolower('google'))|
 {
 if($_COOKIE['cookieconsent']!='true')
 echo '<script type="text/javascript" >
-var cconsent=confirm("Witaj! Wygląda na to, że ta strona używa plików cookie niezbędnych do zapewnienia jej prawidłowego działania oraz plików cookie do śledzenia, aby zrozumieć, w jaki sposób z nią współdziałasz. Jeśli akceptujesz to, wciśnij OK aby przejść do strony. W przeciwnym wypadku wciśnij Cancel (Anuluj).");
+var cconsent=confirm("Hello! This site appears to use cookies necessary to ensure its proper functioning and tracking cookies to understand how you interact with it. If you accept this, press OK to go to the page. Otherwise, press Cancel.");
 if(cconsent) document.cookie = "cookieconsent=true; expires=Wed, 05 Aug 2040 23:00:00 UTC";
 else
 window.location.href="about:blank";
