269629745592cdbec3f073c804a8da11f0acc89d
silosbc <141883892+silosbc@users.noreply.github.com>
2023-08-12 01:48:25 +0200

    Update cookieconsent.php

--- cookieconsent.php
+++ cookieconsent.php
@@ -1,5 +1,5 @@
 <?php
-
+//include this code between the <head></head> tags or just after the <body> tag
 if(empty($_COOKIE['thesimplestcookieconsentplugin']))
 echo '<script type="text/javascript" >
 var cconsent=confirm("Hello! This site appears to use cookies necessary to ensure its proper functioning and tracking cookies to understand how you interact with it. If you accept this, press OK to go to the page. Otherwise, press Cancel.");
